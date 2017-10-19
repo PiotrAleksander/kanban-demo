72193a88c209aeb70ddf7a25bf005896aa92af91
Mrzyglowski <mrzyglop@emea.roche.com>
2017-10-19 15:55:24 +0200

    ContactApps and container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import KanbanBoard from './KanbanBoard';
+import ContactsAppContainer from './Contacts/ContactsAppContainer';
 import './index.css';
 
 let cardsList = [
@@ -38,4 +39,14 @@ let cardsList = [
   },
 ];
 
+let contacts = [
+  { name: "Cassio Zen", email: "cassiozen@gmail.com" },
+  { name: "Dan Abramov", email: "gaearon@somewhere.com" },
+  { name: "Pete Hunt", email: "floydophone@somewhere.com" },
+  { name: "Paul O’Shannessy", email: "zpao@somewhere.com" },
+  { name: "Ryan Florence", email: "rpflorence@somewhere.com" },
+  { name: "Sebastian Markbage", email: "sebmarkbage@here.com" },
+];
+
 ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+ReactDOM.render(<ContactsAppContainer />, document.getElementById('contactsApp'));
