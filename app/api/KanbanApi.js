54e12e20a374e58d5e8ca9c559ecbce39fbe751d
Piotr Aleksander <piotrhugonow@gmail.com>
2017-11-07 23:05:35 +0100

    changed credentials

--- app/api/KanbanApi.js
+++ app/api/KanbanApi.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch';
 import 'babel-polyfill';
 
-const API_URL = 'http://kanbanapi.pro-react.com';
+const API_URL = 'http://localhost:3000/api';
 const API_HEADERS = {
   'Content-Type': 'application/json',
   /*
@@ -9,7 +9,7 @@ const API_HEADERS = {
    * your middle name, your favorite animal, your superpower of choice...
    * An unique authorization will allow you to have your own environment for cards and tasks
    */
-  Authorization: 'stringIlike'
+  Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU5ZmM1Nzc5YjI5Y2EyMzczYzI2NzZkOSIsInVzZXJuYW1lIjoibXJ6eWdsb3N6Iiwic2NvcGUiOiJhZG1pbiIsImlhdCI6MTUxMDA5MTU1NSwiZXhwIjoxNTEwMDk1MTU1fQ.8LsimaSk_zEgVF7XE3cQxggIBktl2slBsXLxDeo36P8'
 }
 
 let KanbanAPI = {
