904a9db35d8e4d9c4a63be70f1aa87952d9e4212
Mrzyglowski <mrzyglop@emea.roche.com>
2017-11-29 10:15:38 +0100

    mui login added

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import AuthActionCreators from '../actions/AuthActionCreators';
 import AuthStore from '../stores/AuthStore';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
@@ -6,69 +10,72 @@ import createBrowserHistory from 'history/lib/createBrowserHistory';
 const history = createBrowserHistory();
 
 class Home extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { 
+        username: '',
+        password: '',
+        error: false
+      }
+  }
 
+  componentDidMount () {
+    AuthActionCreators.isLogged();
+  }
+
+  _onAuthChange(auth) {
+    this.setState(auth);
+
+    if(this.state.loggedIn){
+      history.push('/board');
+    }
+  }
+  
+  handleSubmit(e){
+    e.preventDefault();
+    AuthActionCreators.login(
+      this.state.username,
+      this.state.password
+    );
+  }
+
+  render() {
+    var errorMessage;
+    if (this.state && this.state.error) {
+      errorMessage = (
+        <div className='state-error' style={{ paddingBottom: 16 }}>
+          { this.state.error }
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        { errorMessage }
+        <MuiThemeProvider>
+        <div>
+        <AppBar
+           title="Login"
+         />
+         <TextField
+           hintText="Podaj login/email"
+           floatingLabelText="Login"
+           onChange = {(event,newValue) => this.setState({username:newValue})}
+           />
+         <br/>
+           <TextField
+             type="password"
+             hintText="Podaj hasło"
+             floatingLabelText="Hasło"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={{margin: 15}} onClick={(event) => this.handleSubmit(event)}/>
+       </div>
+       </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default Home;
-
-var Login = React.createClass({  
-    componentDidMount () {
-      this.listenTo(AuthStore, this._onAuthChange);
-    },
-  
-    _onAuthChange(auth) {
-      this.setState(auth);
-  
-      if(this.state.loggedIn){
-        history.push('/board');
-      }
-    },
-  
-    _handleSubmit(event) {
-      event.preventDefault();
-  
-      AuthActionCreators.login(
-        React.findDOMNode(this.refs.username).value,
-        React.findDOMNode(this.refs.password).value
-      );
-    },
-  
-    render() {
-      var errorMessage;
-      if (this.state.error) {
-        errorMessage = (
-          <div className='state-error' style={{ paddingBottom: 16 }}>
-            { this.state.error }
-          </div>
-        );
-      }
-  
-      var formContent;
-      if (this.state.user) {
-        formContent = (
-          <div>
-            <p>
-              Jesteś zalogowany jako <strong>{ this.state.user.name }</strong>.
-            </p>
-          </div>
-        );
-      } else {
-        formContent = (
-          <div>
-            { errorMessage }
-            Login: <input ref="username" style={{ maxWidth: "100%" }} type="text" />
-            <br/>
-            Hasło: <input ref="password" style={{ maxWidth: "100%" }} type="password" />
-            <br/>
-            <button onClick={ this.handleLogout }>Log In</button>
-          </div>
-        );
-      }
-      return (
-        <form onSubmit={this._handleSubmit}>
-          { formContent }
-        </form>
-      );
-    }
-  });
-  module.exports = Login;
